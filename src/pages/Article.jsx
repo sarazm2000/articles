9557d8fc0819d0724336773c782b0522cce2e8be
Sara <sara@carriot.ir>
2023-01-23 19:20:47 +0330

    add article page

--- src/pages/Article.jsx
+++ src/pages/Article.jsx
@@ -1,15 +1,18 @@
 import React, { useEffect, useState } from "react";
 import { useRecoilValue, useRecoilState } from "recoil";
+import Navbar from "../components/Navbar";
 import { slugState } from "./Home";
+import "./article.css";
 
 const Article = () => {
   const [article, setArticle] = useState();
+  const [comments, setComments] = useState();
   const slug = useRecoilValue(slugState);
 
+  let tempSlug =
+    "If-we-quantify-the-alarm-we-can-get-to-the-FTP-pixel-through-the-online-SSL-interface!-120863";
   const fetchData = () => {
-    return fetch(
-      `https://api.realworld.io/api/articles/If-we-quantify-the-alarm-we-can-get-to-the-FTP-pixel-through-the-online-SSL-interface!-120863`
-    )
+    return fetch(`https://api.realworld.io/api/articles/${tempSlug}`)
       .then((response) => response.json())
       .then((data) => setArticle(data.article));
   };
@@ -17,25 +20,56 @@ const Article = () => {
   useEffect(() => {
     fetchData();
   }, []);
-  console.log(article);
+
+  const fetchComments = () => {
+    return fetch(`https://api.realworld.io/api/articles/${tempSlug}/comments`)
+      .then((response) => response.json())
+      .then((data) => setComments(data.comments));
+  };
+
+  useEffect(() => {
+    fetchComments();
+  }, []);
+
+  console.log(comments);
   return (
     <div className="article-page">
-      <div className="favorite">
-        <span className="heart">❤️️</span>
-        <span>{article.favoritesCount}</span>
+      <Navbar />
+      <div className="article-title">
+        <div className="favorite">
+          <span className="heart">❤️️</span>
+          <span>{article.favoritesCount}</span>
+        </div>
+        {article.title}
+        <div className="author">
+          <img className="author-img" src={article.author.image} alt="avatar" />
+          <div className="author-name">{article.author.username}</div>
+        </div>
+        <div className="date">
+          <span className="create-date">created at: {article.createdAt}</span>
+        </div>
       </div>
-      <div className="author">
-        <img className="author-img" src={article.author.image} alt="avatar" />
-        <div className="author-name">{article.author.username}</div>
-      </div>
-      <div className="date">
-        <span className="create-date">created at: {article.createdAt}</span>
-      </div>
-      <div className="article-title">{article.title}</div>
+
       <div className="body">{article.body.replaceAll("\\n", "\n")}</div>
-      {article.tagList.map((tag) => (
-        <span className="tag">{tag}</span>
-      ))}
+      <div className="tag-container">
+        {article.tagList.map((tag) => (
+          <span className="tag">{tag}</span>
+        ))}
+      </div>
+      <div className="comments-container">
+        <hr />
+        <div className="comment-title">Comments</div>
+        {comments.length ? (
+          comments.map((comment) => <div className="comment">{comment}</div>)
+        ) : (
+          <div className="comment">There is no comment yet!</div>
+        )}
+        <br />
+        <div className="add-comment">
+          <textarea name="comment" id="comment" cols="70" rows="4"></textarea>
+          <div className="comment-button">Add</div>
+        </div>
+      </div>
     </div>
   );
 };
